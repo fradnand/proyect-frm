86ee15cda8b0c70533ac0a6b55f194cbb6aeeff2
Fernando Rodriguez <frod6872@gmail.com>
2021-06-29 00:04:59 -0400

    adicionando cards

--- src/components/pages/Inicio.js
+++ src/components/pages/Inicio.js
@@ -1,10 +1,53 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const inicio = () => {
     return (
-        <div>
-            <h1>Esta es la pagina de Inicio</h1>
+
+        <div className="row row-cols-md-4 justify-content-center align-items-center">
+            <div className="col">
+                <div className="card h-100">
+                    <Link to='/'>
+                        <img src='./card1.jpg' width='320'/>
+                    </Link>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    <div className="card-footer">
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <Link to='/'>
+                        <img src='./card2.jpg' width='320'/>
+                    </Link>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    <div className="card-footer">
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <Link to='/'>
+                        <img src='./card3.jpg' width='320'/>
+                    </Link>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    <div className="card-footer">
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
