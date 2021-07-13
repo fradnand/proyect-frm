8a902edbb1667098db2f4e21d6519aace74defec
Fernando Rodriguez <frod6872@gmail.com>
2021-07-13 18:49:25 -0400

    creando api pokemon en lista

---
+++ src/components/Navbar/Navbar2.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './style.css';
+
+function Navbar2() {
+    return (
+        <div className="Navbar2">
+            Pokemon API
+        </div>
+    );
+}
+
+export default Navbar2;
